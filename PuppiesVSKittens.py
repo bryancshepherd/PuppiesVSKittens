9d3dd4de01a5b4cf8bfcc3cb202a81657169a6b5
Bryan Shepherd <bryan.c.shepherd@gmail.com>
2015-12-30 09:22:14 -0500

    Fix bug with reset at midnight

--- PuppiesVSKittens.py
+++ PuppiesVSKittens.py
@@ -7,11 +7,8 @@ import os
 # import RPi.GPIO as GPIO ## Import GPIO library
 # GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 
-# thingGroupA = red = ['puppies', 'puppy', 'dog']
-# thingGroupB = green = ['kittens', 'kitty', 'cat']
-
-thingGroupA = red = ['and']
-thingGroupB = green = ['but']
+thingGroupA = red = ['puppies', 'puppy', 'dog']
+thingGroupB = green = ['kittens', 'kitty', 'cat']
 
 thingGroups = thingGroupA + thingGroupB 
 
@@ -28,18 +25,17 @@ while True:
 
     if not 'totalThingsCounts' in locals(): 
         totalThingsCounts = {thingGroups[i]:0 for i in range(0,len(thingGroups))}
-        
-    r = praw.Reddit('Term tracker by u/mechanicalreddit') # Change 'yourname' to your Reddit username
 
     try:
+        r = praw.Reddit('Term tracker by u/mechanicalreddit') # Change 'yourname' to your Reddit username
         # Rough estimates suggest that there are around 20 new comments per second
-        allComments = r.get_comments('all', limit=500) # The maximum number of comments that can be fetched at a time is 1000
+        allComments = r.get_comments('all', limit=750) # The maximum number of comments that can be fetched at a time is 1000
+        commentDetails = [comment for comment in allComments]
         
     except:
         print("Something didn't work right in fetching the comments")
     
     else:
-        commentDetails = [comment for comment in allComments]
         [print(comment.created) for comment in commentDetails[0:5]] 
 
         if 'timeBoundary' in locals(): # To prevent counting comments twice
@@ -67,14 +63,15 @@ while True:
         greenIntensity = sumThingGroupB/sumOfThings
 
         # Reset the counts at midnight
-        if not 'previousHour' in locals():
-            previousHour = 0
+        if 'previousHour' in locals():
+            if previousHour > datetime.datetime.now().hour:
+                sumThingGroupA = sumThingGroupB = redIntensity = greenIntensity = 0
+                totalThingsCounts = {thingGroups[i]:0 for i in range(0,len(thingGroups))}
+                previousHour = datetime.datetime.now().hour
+                
+            else:
+                previousHour = datetime.datetime.now().hour
         else:
-            previousHour = datetime.datetime.now().hour
-        
-        if previousHour > datetime.datetime.now().hour:
-            sumThingGroupA = sumThingGroupB = redIntensity = greenIntensity = 0
-            totalThingsCounts = {thingGroups[i]:0 for i in range(0,len(thingGroups))}
             previousHour = datetime.datetime.now().hour
 
     # Lighting management
@@ -86,15 +83,15 @@ while True:
 
     
     # Temporary loop management
-    print('sumThingGroupA (and): ' + str(sumThingGroupA))
-    print('sumThingGroupB (but): ' + str(sumThingGroupB))
+    print('sumThingGroupA (Dogs): ' + str(sumThingGroupA))
+    print('sumThingGroupB (Cats): ' + str(sumThingGroupB))
     print('totalThingsCounts: ' + str(totalThingsCounts))
     print('redIntensity: ' + str(redIntensity))
     print('greenIntensity: ' + str(greenIntensity))
     print('timeBoundary: ' + str(timeBoundary))
     print('previousHour: ' + str(previousHour))
     print('current hour: ' + str(datetime.datetime.now().hour))
-    time.sleep(60)
+    time.sleep(120)
 
 
 
